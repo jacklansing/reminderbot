0308e6e4d499b06d3682c948ee723580461b08aa
Jack Lansing <jack.lansing@live.com>
2021-06-30 13:02:56 -0400

    should not have been private func

---
+++ config/prod.exs
@@ -0,0 +1,22 @@
+Code.require_file("./config.exs")
+import Config
+alias ReminderBot.Scheduler
+alias ReminderBot.Scheduler.Task
+
+config :reminder_bot,
+  environment: Mix.env(),
+  ecto_repos: [ReminderBot.Repo]
+
+config :nostrum,
+  token: System.get_env("DISCORD_BOT_TOKEN"),
+  num_shards: :auto
+
+config :reminder_bot, ReminderBot.Scheduler,
+  jobs: [
+    {"0 14 * * 1,3,5", {Task, :send_super_early, []}},
+    {"0 17 * * 1,3,5", {Task, :send_early, []}},
+    {"0 22 * * 1,3,5", {Task, :send_late, []}},
+    {"0 4 * * 2,4,6", {Task, :send_super_late, []}}
+  ]
+
+config :reminder_bot, ReminderBot.Repo, database: "./db/reminder_bot_data.db"
--- lib/reminder_bot_discord/scheduler/task.ex
+++ lib/reminder_bot_discord/scheduler/task.ex
@@ -1,8 +1,8 @@
 defmodule ReminderBot.Scheduler.Task do
   alias ReminderBot.Scheduler.ScheduledServices
 
-  defp send_super_early, do: ScheduledServices.send_gw_reminders("super_early")
-  defp send_early, do: ScheduledServices.send_gw_reminders("early")
-  defp send_late, do: ScheduledServices.send_gw_reminders("late")
-  defp send_super_late, do: ScheduledServices.send_gw_reminders("super_late")
+  def send_super_early, do: ScheduledServices.send_gw_reminders("super_early")
+  def send_early, do: ScheduledServices.send_gw_reminders("early")
+  def send_late, do: ScheduledServices.send_gw_reminders("late")
+  def send_super_late, do: ScheduledServices.send_gw_reminders("super_late")
 end
